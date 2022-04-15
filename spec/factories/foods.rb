8c2cd993fedf20c15e43ec05cc1ed16b564868af
mhnaufal <aal.pirates16@gmail.com>
2022-04-15 03:05:10 +0000

    Generate model for Food latest

---
+++ spec/factories/foods.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :food do
+    name { "Nasi Uduk" }
+    description { "Betawi style steamed rice cooked in coconut milk. Delicious!" }
+    price { 10000.0 }
+    category_id { "all" }
+  end
+end
